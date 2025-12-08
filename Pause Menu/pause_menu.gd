77699c825d70a326057c8c4ca7fcdcf2c5e0f234
Cameron-Hise <chise001@odu.edu>
2025-12-07 23:55:07 -0500

    Updated Pause Menu
    Adds a pause menu with resume, reset, and quit functionality.

---
+++ Pause Menu/pause_menu.gd
@@ -0,0 +1,37 @@
+extends Control
+
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	get_node("PauseButtons").position = Vector2(-999,-999)
+	$AnimationPlayer.play_backwards("pause")
+
+func pause():
+	get_tree().paused = true
+	get_node("PauseButtons").position = Vector2(85.5,36.5)
+	$AnimationPlayer.play("pause")
+
+func testEsc():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _process(delta):
+	testEsc()
