eaa6c170e4e9b47aa5a7d620deac5c7675342776
Qwazwazz <mstoe001@odu.edu>
2025-12-08 04:13:30 -0500

    world updated with testing spawn areas

---
+++ Enemies/Boss/elite.gd
@@ -0,0 +1,83 @@
+extends CharacterBody2D
+
+const HIT_BURST_PARTICLE = preload("res://effects/hit_burst_effect.tscn")
+const FODDER_BURST_PARTICLE = preload("res://Effects/fodder_burst_effect.tscn")
+const FODDER_SPAWN = preload("res://Enemies/Floaty/floater.tscn")
+
+var game: Game = load("res://Scripts/game.tres")
+
+@export var walk_speed: = 80.0
+@export var attack_range: = 32.0
+@export var retreat_range: = 25.0
+@export var stats: Stats
+
+@onready var healthbar: ProgressBar = $Healthbar
+@onready var anchor: Node2D = $Anchor
+
+@onready var hit_sound_effect: AudioStreamPlayer = $HitSoundEffect
+@onready var skeleton_targeter: SkeletonTargeter = $SkeletonTargeter
+@onready var hurtbox: Hurtbox = $Anchor/Hurtbox
+@onready var hitbox: Hitbox = $Anchor/Hitbox
+@onready var effect_marker_2d: Marker2D = $EffectMarker2D
+@onready var unit_mover: UnitMover = $UnitMover
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
+
+func _ready() -> void:
+	stats = stats.duplicate()
+	healthbar.stats = stats
+	hurtbox.hurt.connect(_on_hurt.call_deferred)
+
+func _physics_process(delta: float) -> void:
+	#if position.y <= -50: queue_free()
+	#if position.x <= -50 or position.x >= 1394: queue_free()
+	
+	var state = playback.get_current_node()
+	match state:
+		"ChaseState":
+			walk_speed = 80.0
+			var input_x: float = skeleton_targeter.get_direction_to_skeleton()
+			if input_x != 0.0:
+				unit_mover.apply_flip(-input_x)
+				velocity.x = input_x * walk_speed
+			move_and_slide()
+		"RetreatState":
+			walk_speed = 120.0
+			
+			var input_x: float = -skeleton_targeter.get_direction_to_skeleton()
+			if input_x != 0.0:
+				unit_mover.apply_flip(-input_x)
+				velocity.x = input_x * walk_speed
+			move_and_slide()
+		"AttackState":
+			pass
+		"HitState":
+			unit_mover.apply_friction(delta, unit_mover.hit_friction * 0.01)
+			move_and_slide()
+
+func die() -> void:
+	var fodder_burst_effect = FODDER_BURST_PARTICLE.instantiate()
+	get_tree().current_scene.add_child(fodder_burst_effect)
+	fodder_burst_effect.global_position = effect_marker_2d.global_position
+	
+	var fodder_spawn = FODDER_SPAWN.instantiate()
+	get_tree().current_scene.add_child(fodder_spawn)
+	fodder_spawn.global_position = anchor.global_position
+	
+	game.kills += 1
+	queue_free()
+
+func _on_hurt(other_hitbox: Hitbox) -> void:
+	#hit_sound_effect.play()
+	var hit_burst_particle = HIT_BURST_PARTICLE.instantiate()
+	get_tree().current_scene.add_child(hit_burst_particle)
+	var previous_health = stats.health
+	hit_burst_particle.global_position = effect_marker_2d.global_position
+	stats.health -= other_hitbox.damage
+	unit_mover.apply_knockback(other_hitbox)
+	if previous_health > 0:
+		playback.start("HitState")
+	
+	if stats.health <= 0 and previous_health > 0: 
+		game.kills += 1
+		playback.travel("DieState")
---
+++ Environment/GraveEnvironment/spawn_point.gd
@@ -0,0 +1,50 @@
+class_name SpawnPoint extends Node2D
+
+@export var activation_area: Area2D
+@onready var notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
+var player_inside := false
+
+func _ready():
+	if activation_area:
+		activation_area.body_entered.connect(_on_body_entered)
+		activation_area.body_exited.connect(_on_body_exited)
+
+	notifier.screen_entered.connect(_on_screen_entered)
+	notifier.screen_exited.connect(_on_screen_exited)
+
+	_update_group_state()
+
+
+func _on_body_entered(body):
+	if body is Skeleton:
+		print("Player has entered the spawning area")
+		player_inside = true
+		_update_group_state()
+
+func _on_body_exited(body):
+	if body is Skeleton:
+		print("Player has left the spawning area")
+		player_inside = false
+		_update_group_state()
+
+
+func _on_screen_entered():
+	_update_group_state()
+
+func _on_screen_exited():
+	_update_group_state()
+
+
+func _update_group_state():
+	# Active if player inside AND off-screen
+	if player_inside and !notifier.is_on_screen():
+		add_to_group("active_spawn_points")
+	else:
+		remove_from_group("active_spawn_points")
+
+
+func spawn_enemy(enemy_scene: PackedScene):
+	var enemy = enemy_scene.instantiate()
+	get_tree().current_scene.add_child(enemy)
+	enemy.global_position = global_position
